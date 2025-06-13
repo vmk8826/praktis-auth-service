c8810e3d1411f24fc244823394162c65484934d3
vishnu bharadwaj <vmishra@alpha-sense.com>
2025-06-13 11:51:43 +0530

    fix: add logout route

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import { RequestHandler, Router } from "express";
-import { signup, login, checkAuth } from "../controllers/auth.controllers";
+import { signup, login, checkAuth, logout } from "../controllers/auth.controllers";
 
 const authRoutes = Router();
 
 authRoutes.post("/signup", signup as RequestHandler);
 authRoutes.get("/login", login as RequestHandler);
 authRoutes.get("/checkAuth", checkAuth as RequestHandler);
+authRoutes.get("/logout", logout as RequestHandler);
 
 
 export default authRoutes;
